b704a7c04890553696dcbc99100d820bc7485255
oleg-ch <oleg@bets.io>
2019-05-18 12:13:17 +0300

    Authenticate user

--- lib/rumbl_web.ex
+++ lib/rumbl_web.ex
@@ -23,7 +23,8 @@ defmodule RumblWeb do
 
       import Plug.Conn
       import RumblWeb.Gettext
-      import RumblWeb.Auth, only: [authenticate_user: 2] # New import
+      # New import
+      import RumblWeb.Auth, only: [authenticate_user: 2]
       alias RumblWeb.Router.Helpers, as: Routes
     end
   end
@@ -50,7 +51,8 @@ defmodule RumblWeb do
       use Phoenix.Router
       import Plug.Conn
       import Phoenix.Controller
-      import RumblWeb.Auth, only: [authenticate_user: 2] # New import
+      # New import
+      import RumblWeb.Auth, only: [authenticate_user: 2]
     end
   end
 
